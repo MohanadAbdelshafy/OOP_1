4908c8b55ad5d1d62dfcc3690ce2da2bac78d62a
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-13 02:29:40 +0300

    Question 1

--- OOP_01/Program.cs
+++ OOP_01/Program.cs
@@ -1,10 +1,41 @@
 ﻿namespace OOP_01
 {
+    #region Question 1
+    //public struct DeliveryAddress
+    //{
+    //    public string City;
+    //    public string Street;
+    //}
+    //public class Customer
+    //{
+    //    public string name;
+    //}
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
         {
-        
+            #region Question 1 Object creation
+            //DeliveryAddress address = new DeliveryAddress();
+
+            //address.City = "Cairo";
+            //address.Street = "Main";
+            //DeliveryAddress address2 = address;
+            //address2.City = "Alexandria";
+            //Console.WriteLine(address2.City);
+            //Console.WriteLine(address.City);
+            ////struct is a value type the value is simply just copied to the other variable 
+
+            //Customer customer1 = new Customer();
+            //customer1.name = "Ahmed";
+
+            //Customer customer2 = customer1;
+
+            //customer2.name = "Ali";
+
+            //Console.WriteLine(customer1.name);
+            ////class is a reference type,So both customer references refers to the same object
+            #endregion
         }
     }
 }
