a063d7f0b8a4a5ceb3c77c1a55f636fba4ea9b95
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-13 02:33:29 +0300

    Part2 2. Shipment struct

--- OOP_01/Program.cs
+++ OOP_01/Program.cs
@@ -71,6 +71,132 @@
     //    }
     //}
     #endregion
+    #region Part2 2. Shipment struct
+    //public struct Shipment
+    //{
+    //    private string trackingCode;
+    //    private string description;
+    //    private double weight;
+    //    private decimal deliveryFee;
+
+    //    public DeliveryAddress Destination { get; set; }
+
+    //    public string TrackingCode
+    //    {
+    //        get { return trackingCode; }
+    //    }
+
+    //    public string Description
+    //    {
+    //        get { return description; }
+    //        set
+    //        {
+    //            if (!string.IsNullOrWhiteSpace(value))
+    //            {
+    //                description = value;
+    //            }
+    //        }
+    //    }
+
+    //    public double Weight
+    //    {
+    //        get { return weight; }
+    //        set
+    //        {
+    //            if (value > 0)
+    //            {
+    //                weight = value;
+    //            }
+    //        }
+    //    }
+
+    //    public decimal DeliveryFee
+    //    {
+    //        get { return deliveryFee; }
+    //        private set
+    //        {
+    //            if (value > 0)
+    //            {
+    //                deliveryFee = value;
+    //            }
+    //        }
+    //    }
+
+    //    public decimal EstimatedCost
+    //    {
+    //        get { return DeliveryFee + (decimal)(Weight * 5); }
+    //    }
+    //    public Shipment(string trackingCode)
+    //    {
+    //        if (string.IsNullOrWhiteSpace(trackingCode))
+    //        {
+    //            Console.WriteLine("cannot be empty");
+    //        }
+
+    //        this.trackingCode = trackingCode;
+    //        description = "Unknown";
+    //        weight = 1;
+    //        deliveryFee = 50;
+    //        Destination = new DeliveryAddress("Unknown", "Unknown", 0);
+    //    }
+
+    //    public Shipment(
+    //        string trackingCode,
+    //        string description,
+    //        double weight,
+    //        decimal deliveryFee,
+    //        DeliveryAddress destination)
+    //    {
+    //        if (string.IsNullOrWhiteSpace(trackingCode))
+    //        {
+    //            throw new ArgumentException("Tracking code cannot be empty.");
+    //        }
+
+    //        this.trackingCode = trackingCode;
+
+    //        this.description = "Unknown";
+    //        this.weight = 1;
+    //        this.deliveryFee = 50;
+
+    //        if (!string.IsNullOrWhiteSpace(description))
+    //        {
+    //            this.description = description;
+    //        }
+
+    //        if (weight > 0)
+    //        {
+    //            this.weight = weight;
+    //        }
+
+    //        if (deliveryFee > 0)
+    //        {
+    //            this.deliveryFee = deliveryFee;
+    //        }
+
+    //        Destination = destination;
+
+
+    //    }
+    //    public void UpdateDeliveryFee(decimal newFee)
+    //    {
+    //        if (newFee > 0)
+    //        {
+    //            DeliveryFee = newFee;
+    //        }
+    //    }
+
+    //    public void PrintShipment()
+    //    {
+    //        Console.WriteLine("Tracking Code: " + TrackingCode);
+    //        Console.WriteLine("Description: " + Description);
+    //        Console.WriteLine("Weight: " + Weight + " KG");
+    //        Console.WriteLine("Delivery Fee: " + DeliveryFee + " EGP");
+    //        Console.WriteLine("Destination: " + Destination.GetFullAddress());
+    //        Console.WriteLine("Estimated Cost: " + EstimatedCost + " EGP");
+    //    }
+    //}
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
