ac180fb21e4466a950c8e023635d30398e3b019c
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-13 02:34:09 +0300

    part 2 5 Delivery center struct

--- OOP_01/Program.cs
+++ OOP_01/Program.cs
@@ -197,6 +197,67 @@
     //}
 
     #endregion
+    #region part 2 5 Delivery center struct
+    //public struct DeliveryCenter
+    //{
+    //    private Shipment[] shipments;
+
+    //    public DeliveryCenter()
+    //    {
+    //        shipments = new Shipment[10];
+    //    }
+
+    //    public Shipment this[int index]
+    //    {
+    //        get
+    //        {
+    //            if (index >= 0 && index < shipments.Length)
+    //            {
+    //                return shipments[index];
+    //            }
+
+    //            return default;
+    //        }
+
+    //        set
+    //        {
+    //            if (index >= 0 && index < shipments.Length)
+    //            {
+    //                shipments[index] = value;
+    //            }
+    //        }
+    //    }
+    //    public Shipment this[string trackingCode]
+    //    {
+    //        get
+    //        {
+    //            for (int i = 0; i < shipments.Length; i++)
+    //            {
+    //                if (shipments[i].TrackingCode == trackingCode)
+    //                {
+    //                    return shipments[i];
+    //                }
+    //            }
+
+    //            return default;
+    //        }
+    //    }
+    //    public bool AddShipment(Shipment shipment)
+    //    {
+    //        for (int i = 0; i < shipments.Length; i++)
+    //        {
+    //            if (string.IsNullOrEmpty(shipments[i].TrackingCode))
+    //            {
+    //                shipments[i] = shipment;
+    //                return true;
+    //            }
+    //        }
+
+    //        return false;
+    //    }
+    //}
+
+    #endregion
     internal class Program
     {
         static void Main(string[] args)
